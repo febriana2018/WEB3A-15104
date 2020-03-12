62280a635fc0deeb0f1ff5f4e596e1aed72fa745
Febriana <febriana2018@mail.ugm.ac.id>
2020-03-12 15:01:36 +0700

    first antd

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './App.css';
+import { Card, Col, Row } from 'antd';
+import { Layout, Menu } from 'antd';
+import { Rate } from 'antd';
+import { Steps } from 'antd';
+//import { Pagination } from 'antd';
+const { Step } = Steps;
+const { Header} = Layout;
+
+function App(){
+
+  const cardData = [
+    {
+      title:"Febriana P",
+      job:"FrontEnd Dev",
+      finish:"Design tampilan website dengan Ant Design",
+      progress:"Memperbarui tampilan sesuai permintaan customer",
+      waiting:"Projek Selanjutnya"
+    },
+    {
+      title:"Ana Sari",
+      job:"Project Manager",
+      finish:"Membagi tugas sesuai deskjob",
+      progress:"Memantau progress tiap divisi",
+      waiting:"Projek Selanjutnya"
+    },
+    {
+      title:"Pamungkasari",
+      job:"UI/UX",
+      finish:"Membuat prototipe tampilan website",
+      progress:"Memberi perubahan yang perlu",
+      waiting:"Projek Selanjutnya"
+    }
+  ]
+
+  return (
+    // <div>
+    //   {cardData.map(data =>
+    //     <div className="card">
+    //       <p className="title">{data.title}</p>
+    //       <p>{data.job}</p>
+    //     </div>
+    //     )}
+    // </div>
+  <Layout className="layout">
+    <Header>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">Menu 1</Menu.Item>
+        <Menu.Item key="2">Menu 2</Menu.Item>
+        <Menu.Item key="3">Menu 3</Menu.Item>
+      </Menu>
+    </Header>
+
+    <div className="site-card-wrapper">
+      <Row gutter={[8,8]}>
+        {cardData.map(data =>
+        <Col span={8}>
+          <Card title="Data Nama" bordered={false}>
+            <p className="title">{data.title}</p>
+            <p>{data.job}</p>
+            <p><Rate allowHalf defaultValue={4} /> </p>
+          </Card>
+
+          <Card title="Pengerjaan" bordered={false}>
+            <Steps direction="vertical" current={1}>
+              <Step title="Finished" description={data.finish}/>
+              <Step title="In Progress" description={data.progress}/>
+              <Step title="Waiting" description={data.waiting}/>
+            </Steps>
+          </Card>
+
+        </Col>
+        )}
+        </Row>
+    </div>
+    
+  </Layout>
+  );
+
+}
+
+export default App;
