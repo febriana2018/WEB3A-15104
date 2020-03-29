12cdcfd12b9d261824a354b39fa77d9990bbb549
Febriana <febriana2018@mail.ugm.ac.id>
2020-03-30 04:28:57 +0700

    Updates

---
+++ precache-manifest.562dccb95773e8cc810f282d5ee8f155.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a48c867811063ec36594d0de0907f2",
+    "url": "/WEB3A-15104-Febriana-REACT/index.html"
+  },
+  {
+    "revision": "0db79cc2465556d180ab",
+    "url": "/WEB3A-15104-Febriana-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "984df8c0c6fcca14ade3",
+    "url": "/WEB3A-15104-Febriana-REACT/static/css/main.6c57eb45.chunk.css"
+  },
+  {
+    "revision": "0db79cc2465556d180ab",
+    "url": "/WEB3A-15104-Febriana-REACT/static/js/2.ba773f6b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/WEB3A-15104-Febriana-REACT/static/js/2.ba773f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984df8c0c6fcca14ade3",
+    "url": "/WEB3A-15104-Febriana-REACT/static/js/main.b6c206bf.chunk.js"
+  },
+  {
+    "revision": "440afbe2fef4b5dc3007",
+    "url": "/WEB3A-15104-Febriana-REACT/static/js/runtime-main.4b69b492.js"
+  }
+]);
